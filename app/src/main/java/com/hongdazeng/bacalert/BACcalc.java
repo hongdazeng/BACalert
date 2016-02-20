2fb9cf5a01c5bdbaa0b25d80d2e5984d8a807e26
Xu Han <han308@purdue.edu>
2016-02-20 13:11:10 -0500

    added bac class

---
+++ app/src/main/java/com/hongdazeng/bacalert/BACcalc.java
@@ -0,0 +1,45 @@
+package com.hongdazeng.bacalert;
+
+import java.text.DecimalFormat;
+/**
+ * Created by Xu Han on 2/20/2016.
+ *
+ */
+public class BACcalc {
+    private double weight;
+    private int numDrinks;
+    private double gender;
+    //double drinkPeriod;
+    public BACcalc(){
+        weight = (150/2.2406);
+        gender = 0.58;
+        numDrinks = 0;
+    }
+    public void setWeight(int weight){
+        this.weight = (weight/2.2406);
+    }
+    public double getWeight(){
+        return weight;
+    }
+    public void addDrinks(){
+        numDrinks += 1;
+    }
+    //public void setDrinkPeriod(double drinkPeriod){
+        //this.drinkPeriod = drinkPeriod;
+    //}
+    public void changeGender(){
+        if(gender == 0.58){
+            gender = 0.49;
+        }
+        else{
+            gender = 0.58;
+        }
+    }
+    public double getBAC(){
+        double BAC = (0.806* numDrinks * 1.2)/(gender * weight) - (0.017 * 0.25); //0.25 = 15 minutes drink period
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        System.out.println(formatter.format(BAC));
+        return BAC;
+    }
+
+}
